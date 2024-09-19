ca44b110fbeb1eb83e8a39bcde88e813c1719799
julianadslopes <jujulianalopes@live.com>
2024-09-19 14:42:14 -0300

    Aula 01

---
+++ codigo_comentado/main.py
@@ -0,0 +1,87 @@
+# Variáveis são pequenos espaços de memória, utilizados para armazenar e
+# manipular dados. Em Python, os tipos de dados básicos são tipo inteiro
+# (armazena números inteiros), tipo float (armazena números em formato
+# decimal), tipo string (armazena um conjunto de caracteres) e tipo Bool,
+# que armazena True ou False.
+# Cada variável pode armazenar apenas um tipo de dado a cada instante.
+# Em Python, diferentemente de outras linguagens de programação, não é preciso
+# declarar de que tipo será cada variável no início do programa.
+
+
+#  Tipos de Variáveis:
+# int - Número inteiros
+# float - Números decimais
+# String - Cadeia de caracteres (Valores entre aspas)
+# bool - Valores booleanos: True ou False (sempre com a primeira
+# letra maiúscula)
+
+# Variáveis
+# Quando se faz uma atribuição de valor, automaticamente, a variável se
+# torna do chip o do valor armazenado, Como apresentado nos exemplos a seguir.
+
+
+# Exemplos
+
+
+a = 29
+# A variável se torna uma variável do tipo inteira.
+
+
+b = 1.8
+# A variável b se torna uma variável do tipo float.
+
+
+c = 'Ao infinito e além!!!'
+# A variável se torna uma variável do tipo string.
+
+
+# Função Print()
+# Usada para escrever algo na no console
+# Escrevendo na tela uma String
+print('Olá mundo')
+
+# Variável tipo String
+# nome recebe João
+nome = 'João'
+
+# Variável tipo inteiro
+# idade recebe 22
+idade = 22
+
+# Variável tipo float
+# altura recebe 1.75
+altura = 1.75
+
+# Variável tipo Bool
+# resposta recebe True
+resposta = True
+
+
+# Operadores Aritméticos
+# + Operador de adição
+# - Operador de subtração
+# * Operador de Multiplicação
+# / Operador de Divisão
+# // Operador de Divisão Inteira
+# % Operador Resto da Divisão (MOD)
+
+# Operação com variáveis
+n1 = 5
+n2 = 2
+
+# Operações entre N1 e N2
+print('Operações entre', n1, 'e', n2)
+print('Soma', n1 + n2)
+print('Subtralção', n1 - n2)
+print('Multiplicação', n1 * n2)
+print('Divisão', n1 / n2)
+print('Divisão Inteira', n1 // n2)
+print('O quadrado de', n1, 'é', n1 ** n2)
+print('O resto da divisão entre', n1, 'e', n2, 'resulta em', n1 % n2)
+
+# Expressões literais
+primeironome = 'Paulo'
+sobrenome = 'Moraes'
+
+nomecompleto = primeironome + sobrenome
+print(nomecompleto)
